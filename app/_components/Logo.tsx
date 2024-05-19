ea0f8c747075edab1aa585742de967bafecadb1c
abdullahqayid <3boo0odq0v@gmail.com>
2024-05-19 22:48:59 +0300

    setting up custom styles

--- app/_components/Logo.tsx
+++ app/_components/Logo.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function Logo() {
   return (
-    <Link href="/" className="flex bg-red-400 items-center gap-4 z-10">
+    <Link href="/" className="flex items-center gap-4 z-10">
       <Image src="/logo.png" height="60" width="60" alt="The Wild Oasis Logo" />
       <span className="text-xl  font-semibold text-primary-100">
         The Wild Oasis
