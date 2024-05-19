7009b647dfcac27e8b34beb4cd859fcc20adf80d
abdullahqayid <3boo0odq0v@gmail.com>
2024-05-19 22:37:56 +0300

    sitting up folder structure done

---
+++ app/_components/Logo.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export default function Logo() {
+  return (
+    <Link href="/" className="flex bg-red-400 items-center gap-4 z-10">
+      <Image src="/logo.png" height="60" width="60" alt="The Wild Oasis Logo" />
+      <span className="text-xl  font-semibold text-primary-100">
+        The Wild Oasis
+      </span>
+    </Link>
+  );
+}
---
+++ app/about/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function page() {
+  return <div>About The Wild Oasisw</div>;
+}
