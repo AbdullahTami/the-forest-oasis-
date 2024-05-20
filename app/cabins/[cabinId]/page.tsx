e0a704c2995cd38762f744de3914998226265b82
abdullahqayid <3boo0odq0v@gmail.com>
2024-05-21 00:09:09 +0300

    successfully generatinng dynamic metadata

--- app/cabins/[cabinId]/page.tsx
+++ app/cabins/[cabinId]/page.tsx
@@ -2,11 +2,16 @@ import { getCabin } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
-export default async function Page({
-  params,
-}: {
-  params: { cabinId: string };
-}) {
+type Params = {
+  cabinId: string;
+};
+
+export async function generateMetadata({ params }: { params: Params }) {
+  const { name } = await getCabin(params.cabinId);
+  return { title: `Cabin ${name}` };
+}
+
+export default async function Page({ params }: { params: Params }) {
   console.log(params);
   const cabin = await getCabin(params.cabinId);
   const { id, name, maxCapacity, regularPrice, discount, image, description } =
