33806a03bea1417477b1407a7159334751aa526a
abdullahqayid <3boo0odq0v@gmail.com>
2024-05-21 17:12:02 +0300

    setting up page not found error done

--- app/_lib/data-service.ts
+++ app/_lib/data-service.ts
@@ -1,6 +1,7 @@
 import { eachDayOfInterval } from "date-fns";
 import { supabase } from "./supabase";
 import { CabinType } from "@/app/_components/CabinCard";
+import { notFound } from "next/navigation";
 
 /////////////
 // GET
@@ -17,6 +18,7 @@ export async function getCabin(id: string): Promise<CabinType> {
 
   if (error) {
     console.error(error);
+    notFound();
   }
 
   return data;
