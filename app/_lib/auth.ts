6524f6542eb5fcad01ce148e16201dbe4029c02b
abdullahqayid <3boo0odq0v@gmail.com>
2024-05-25 18:09:49 +0300

    successfully setting up auth.js with google provider credentials

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -4,8 +4,8 @@ import Google from "next-auth/providers/google";
 const authConfig = {
   providers: [
     Google({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
       authorization: {
         params: {
           prompt: "consent",
